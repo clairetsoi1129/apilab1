bc9cf7c2dd4b09001baf70c0e3dde2bba16510e5
Claire Tsoi <clairetsoi1129@gmail.com>
2023-03-06 20:29:59 +0000

    Add - Coffee controller with endpoint /coffee and its test cases

--- src/test/java/com/techreturners/apilab1/controller/CoffeeControllerTests.java
+++ src/test/java/com/techreturners/apilab1/controller/CoffeeControllerTests.java
@@ -29,4 +29,34 @@ public class CoffeeControllerTests {
                 .andExpect(MockMvcResultMatchers.content().string
                         (expectedContent));
     }
+
+    @Test
+    public void testGetCoffeeWithName() throws Exception {
+        //Arrange, Act and Assert chained within the following statements
+        String expectedContent = "{\"id\":2,\"name\":\"cappuccino\"}";
+        this.mockMvcController.perform(
+                        //Make a GET request to the '/' endpoint
+                        MockMvcRequestBuilders.get("/coffee?name=cappuccino"))
+                //Matchers are helpful methods that help you write your assertions
+                //According to the requirement, we expect 200 OK Status Code as a Response
+                //According to the requirement, we expect the string as a Response
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json
+                        (expectedContent));
+    }
+
+    @Test
+    public void testGetCoffeeWithoutName() throws Exception {
+        //Arrange, Act and Assert chained within the following statements
+        String expectedContent = "{\"id\":1,\"name\":\"latte\"}";
+        this.mockMvcController.perform(
+                        //Make a GET request to the '/' endpoint
+                        MockMvcRequestBuilders.get("/coffee"))
+                //Matchers are helpful methods that help you write your assertions
+                //According to the requirement, we expect 200 OK Status Code as a Response
+                //According to the requirement, we expect the string as a Response
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json
+                        (expectedContent));
+    }
 }
